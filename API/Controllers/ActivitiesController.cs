7e0cd5824965b21db658e9cbbd689c9c4f9e4f74
Tyler Snyder <tylersnyder619406@gmail.com>
2022-03-06 15:19:25 -0700

    Added ability to Read activity request

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -6,30 +6,25 @@ using Persistence;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Domain;
-
+using Application.Activities;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
+      
         
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] // activities/id
         public async Task<ActionResult<Activity>> GetActivities(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query{Id = id});
         }
 
     }
