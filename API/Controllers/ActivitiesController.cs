fe7a299010ec67a1e68714c2c731f5ab59987bad
Tyler Snyder <tylersnyder619406@gmail.com>
2022-03-07 21:12:16 -0700

    Added Edit Handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,9 +11,7 @@ using Application.Activities;
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
-    {
-      
-        
+    {      
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
@@ -31,6 +29,14 @@ namespace API.Controllers
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
             return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+
+            return Ok(await Mediator.Send(new Edit.Command {Activity = activity}));
         }
 
     }
