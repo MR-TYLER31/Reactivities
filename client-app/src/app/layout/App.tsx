d63737136449dba6aaa48569e08bedf80fff3564
Tyler Snyder <tylersnyder619406@gmail.com>
2022-03-19 13:23:31 -0600

    Finished Mobx activity store

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,7 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
-import agent from '../api/agent';
 import LoadingComponents from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
@@ -13,44 +10,10 @@ import { observer } from 'mobx-react-lite';
 function App() {
   const {activityStore} = useStore()
 
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const [submitting, setSubmitting] = useState(false);
-
   useEffect(() => {
     activityStore.loadActivities();
   },[activityStore])
 
-  function handleCreateOrEditActivity(activity: Activity) {
-    setSubmitting(true);
-
-    if(activity.id) {
-      agent.Activities.update(activity).then(() => {
-        setActivities([...activities.filter(x => x.id !== activity.id), activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    } else {
-      activity.id = uuid();
-      agent.Activities.create(activity).then(() => {
-        setActivities([...activities, activity]);
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    }
-  }
-
-  function handleDeleteActivity(id: string) {
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(x => x.id !== id)]);
-      setSubmitting(false);
-    })
-    
-  }
 
   if (activityStore.loadingInitial) return <LoadingComponents content='Loading app'/>
 
@@ -58,12 +21,7 @@ function App() {
     <>
       <NavBar />   
       <Container style={{marginTop: '7em'}}>
-         <ActivityDashboard 
-         activities={activityStore.activities}
-         createOrEdit={handleCreateOrEditActivity}
-         deleteActivity={handleDeleteActivity}
-         submitting={submitting}
-         />
+         <ActivityDashboard />
       </Container>  
     </>
   );
