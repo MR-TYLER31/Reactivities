99625045dc798f4772e1aeed89176c02643b12e5
Tyler Snyder <tylersnyder619406@gmail.com>
2022-04-11 21:42:40 -0600

    Added fluent validation on create and edit requests

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -16,6 +17,14 @@ namespace Application.Activities
         {
             public Activity Activity { get; set; }
                       
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+            }
         }
 
         public class Handler : IRequestHandler<Command>
