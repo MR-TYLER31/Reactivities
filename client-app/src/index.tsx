89e9230490295cc2f14ceae8866be0c5c965cf24
Tyler Snyder <tylersnyder619406@gmail.com>
2022-04-14 18:19:05 -0600

    added error handle for create activity

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
   <StoreContext.Provider value={store}>
     <BrowserRouter>
-       <App />
+       <App/>
     </BrowserRouter>
   </StoreContext.Provider>,
   document.getElementById('root')
